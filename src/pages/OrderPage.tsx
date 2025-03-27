ee2d2e2b1f7895ace253af37449558585f77e63d
Hacaka288 <olisaelijah7@gmail.com>
2025-03-27 17:13:18 +0100

    idk

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -13,16 +13,16 @@ const OrderPage = () => {
   };
 
   return (
-    <div className="min-h-screen pb-20">
+    <div className="min-h-screen pb-16 md:pb-20">
       <button 
         onClick={handleGoBack}
-        className="inline-flex items-center justify-center h-12 px-8 font-medium tracking-wide text-risen-600 transition duration-200 bg-white rounded-lg hover:bg-risen-50 focus:shadow-outline focus:outline-none m-4"
+        className="inline-flex items-center justify-center h-10 md:h-12 px-4 md:px-8 font-medium tracking-wide text-risen-600 transition duration-200 bg-white rounded-lg hover:bg-risen-50 focus:shadow-outline focus:outline-none m-3 md:m-4"
       >
         Go Back
       </button>
       
-      <section className="scroll-mt-24">
-        <div className="container mx-auto px-3 sm:px-4">
+      <section className="scroll-mt-24 py-5 md:py-16">
+        <div className="container mx-auto px-2 sm:px-4">
           <OrderForm onOrderSuccess={() => setIsThankYouOpen(true)} />
         </div>
       </section>
